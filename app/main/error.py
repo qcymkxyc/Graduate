2afedacaaa3a0f4d9bbc13596d967ec8808d43d6
qcymkxyc <qcymkxyc@163.com>
2018-11-10 15:49:13 +0800

    update

--- app/main/error.py
+++ app/main/error.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 # coding=utf-8
 
-from flask import request,render_template,jsonify
+from flask import request, render_template, jsonify
 from . import main
+
 
 @main.app_errorhandler(404)
 def page_not_found(e):
     if (request.accept_mimetypes.accept_json and
             not request.accept_mimetypes.accept_html):
-            response = jsonify({"error" : "Not Found"})
-            response.status_code = 404
-            return response
-    return render_template("404.html"),404
+        response = jsonify({"error": "Not Found"})
+        response.status_code = 404
+        return response
+    return render_template("404.html"), 404
+
 
 @main.app_errorhandler(403)
 def forbidden(e):
@@ -21,6 +23,7 @@ def forbidden(e):
         response.status_code = 403
         return response
     return render_template('403.html'), 403
+
 
 @main.app_errorhandler(500)
 def internal_server_error(e):
